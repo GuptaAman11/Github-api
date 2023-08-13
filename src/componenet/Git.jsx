acae648cbf4f38cc3d1065166f0bbc4134638643
kings-of-king <amangupta9579@gmail.com>
2023-08-13 15:56:01 +0530

    a

--- src/componenet/Git.jsx
+++ src/componenet/Git.jsx
@@ -1,27 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Git = () => {
 
     const [search , setSearch] = useState("GuptaAman11")
     const [usdata , setUsData]= useState("")
 
+    
+
     const  Data =async ()=>{
+      
       try {
         const userResponse = await fetch(`https://api.github.com/users/${search}`)
         const  userData = await userResponse.json()
         setUsData(userData);
         
+        
       }catch(error){
         console.error(error)
       }
+
+
+
+    
 
     }
   return (
     <div>
       <div>
-        <input type='text'></input>
+        <input type='text' onChange={(e)=>setSearch(e.target.value)} ></input>
         <button onClick={Data}>search</button>
-        <h1>{usdata.avatar_url}</h1>
+        <h1>{usdata.login}</h1>
+        <img src={usdata.avatar_url}/>
       </div>
     </div>
   )
