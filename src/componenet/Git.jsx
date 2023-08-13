05901a0fbea2ff4c76655bd1d754fa634355a82c
kings-of-king <amangupta9579@gmail.com>
2023-08-13 12:48:15 +0530

    a

---
+++ src/componenet/Git.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Git = () => {
+
+    const
+  return (
+    <div>
+      <div>
+        <input type='text'></input>
+        <button onClick={handleseach}>seach</button>
+      </div>
+    </div>
+  )
+}
+
+export default Git
+
